ff4de81051775d10ef1b3195833b17aa140038cd
Tamilselvi459 <tamilselvig459@gmail.com>
2023-04-15 12:09:08 +0530

    assignment done

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -3,6 +3,9 @@ package com.driver;
 public class Boat implements WaterVehicle{
     String name;
     int capacity;
+    public Boat(){
+
+    }
     @Override
     public String getVehicleName() {
         return name;
